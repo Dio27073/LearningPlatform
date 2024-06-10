553873bf8721411f20f2379d069ad38570a5bcf8
Claudio Florio <claudioflorio1@hotmail.com>
2024-06-10 17:08:37 -0400

    login, register, logout functions

--- app.py
+++ app.py
@@ -1,18 +1,67 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_user, logout_user
+
+from models import User, Role, Quiz, Flashcard
 
 app = Flask(__name__)
 
+# SQL Setup
 app.config['SQLALCHEMY_DATABASE_URI'] = ' '
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
+
+# login setup
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User.query.filter_by(email=email).first()
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            flash('login unsuccessful.', 'danger')
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        role_id = request.form['role']
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        user = User(username=username, email=email, password=hashed_password, role_id=role_id)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You can now log in', 'success')
+        return redirect(url_for('login'))
+    roles = Role.query.all()
+    return render_template('register.html', roles=roles)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
